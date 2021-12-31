46d38b1e8b68d38ad77022dbe29e17de01136482
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2021-12-31 20:37:18 +0900

    함수 포인터 공부

---
+++ Cpp_studying/함수_포인터.cpp
@@ -0,0 +1,48 @@
+﻿#include<iostream>
+#include<array>
+
+using namespace std;
+
+bool isOdd(const int &num) {
+	if (num % 2 != 0)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+bool isEven(const int &num) {
+	if (num % 2 == 0)
+	{
+		return true;
+	}
+	else
+	{
+		return false;
+	}
+}
+
+
+void printNum(const array<int, 10> &m_array, bool (*checkFunc)(const int &)) { // 함수 포인터 checkFunc 사용
+	for (int elements : m_array) {
+		if (checkFunc(elements) == true)
+		{
+			cout << elements;
+		}
+	}
+	cout << endl;
+}
+
+int main() {
+
+	array<int, 10> m_array{ 0,1,2,3,4,5,6,7,8,9 };
+
+	printNum(m_array, isOdd); // 13579
+	printNum(m_array, isEven); // 02468
+
+
+	return 0;
+}
