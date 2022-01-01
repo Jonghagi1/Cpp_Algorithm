ee10fd35eaa0a5b81faa9531cda0b285e1e51132
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-01-01 13:26:12 +0900

    OOP 캡슐화, 접근 지정자 공부

---
+++ Cpp_studying/캡슐화.cpp
@@ -0,0 +1,47 @@
+﻿#include<iostream>
+
+using namespace std;
+
+class Date
+{	
+	int m_month;
+	int m_day;
+	int m_year;
+	
+public:				//접근 지정자 (access specifier) // private가 기본
+	void setDate(const int &month_input, const int &day_input, const int &year_input) {
+		m_month = month_input;
+		m_day = day_input;
+		m_year = year_input;
+	}
+
+	void setMonth(const int &month_input) {
+		m_month = month_input;
+	}
+
+	const int &getDay() {
+		return m_day;
+	}
+
+	void copyForm(const Date &original) {
+		m_month = original.m_month;
+		m_day = original.m_day;
+		m_year = original.m_year;
+	}
+};
+
+int main() {
+
+	Date today;
+
+	today.setDate(1, 1, 2022);
+
+	cout << today.getDay() << endl;
+
+	Date copy;
+	copy.copyForm(today);
+
+	cout << copy.getDay();
+
+	return 0;
+}
