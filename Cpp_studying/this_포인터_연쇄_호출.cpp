d00ee9296b1977e064fe9ec66713de8fc22c3688
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-01-01 20:02:34 +0900

    this 포인터, 연쇄 호출 기능 공부

---
+++ Cpp_studying/this_포인터_연쇄_호출.cpp
@@ -0,0 +1,46 @@
+﻿#include<iostream>
+
+using namespace std;
+
+class Calc {
+private:
+	int m_value;
+
+public:
+	Calc(int init_value)
+		: m_value(init_value)
+	{
+
+	}
+
+	Calc &add(int value)
+	{
+		m_value += value;
+		return *this;		// this 포인터를 이용해서 자기 자신을 호출
+	}
+	Calc &sub(int value)
+	{
+		m_value -= value;
+		return *this;		// this 포인터를 이용해서 자기 자신을 호출
+	}
+	Calc &mul(int value)
+	{
+		m_value *= value;
+		return *this;		// this 포인터를 이용해서 자기 자신을 호출
+	}
+
+	void print()
+	{
+		cout << m_value << endl;
+	}
+};
+
+int main() {
+
+	Calc cal(10);
+	cal.add(100).sub(10).mul(2).print(); // this 포인터를 이용한 연쇄 호출 (Member function chaining) 기법
+	// 200
+
+
+	return 0;
+}
