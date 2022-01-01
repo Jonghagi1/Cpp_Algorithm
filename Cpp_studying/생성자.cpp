98bdd637052fc29da79d6f0260ad99cd2ecb47c7
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-01-01 15:18:03 +0900

    생성자 공부

---
+++ Cpp_studying/생성자.cpp
@@ -0,0 +1,28 @@
+﻿#include<iostream>
+
+using namespace std;
+
+class Fraction {
+public:
+	int m_numerator;
+	int m_denominator;
+
+public:
+	Fraction(const int &num_input, const int &denom_input) { // 생성자 ( Constructors )
+		m_numerator = num_input;
+		m_denominator = denom_input;
+	}
+
+	void print() {
+		cout << m_numerator << " / " << m_denominator << endl;
+	}
+};
+
+int main() {
+	Fraction frac(1, 2); // 생성자 호출 // 파라미터가 없으면 Fraction frac 으로 호출
+
+	frac.print();		 // 1 / 2
+
+
+	return 0;
+}
