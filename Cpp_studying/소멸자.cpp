4ed9bc76eb88496a4564e1c4ad67235bb654667d
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-01-01 16:44:25 +0900

    소멸자 공부

---
+++ Cpp_studying/소멸자.cpp
@@ -0,0 +1,35 @@
+﻿#include<iostream>
+
+using namespace std;
+
+class intArray {
+private:
+	int *m_arr = nullptr;
+	int m_length = 0;
+public:
+	intArray(const int &length_in) {
+		m_length = length_in;
+		m_arr = new int[m_length];
+
+		cout << "생성자" << endl;
+	}
+	
+	int getLength() {
+		return m_length;
+	}
+
+	~intArray() {		// 소멸자, vector 내부에는 소멸자 매커니즘이 구현 되어있음
+		delete[] m_arr;
+	}
+};
+
+int main() {
+
+	while (true)
+	{
+		intArray my_arr(10000);
+	}
+
+
+	return 0;
+}
