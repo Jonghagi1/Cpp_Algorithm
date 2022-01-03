9c8042360ad608ff4a4ce14ee2d29ef24cc51d20
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-01-03 21:09:16 +0900

    산술 연산자 오버로딩, 익명 객체 공부

---
+++ Cpp_studying/산술_연산자_오버로딩.cpp
@@ -0,0 +1,57 @@
+﻿#include<iostream>
+
+using namespace std;
+
+class Cents
+{
+private:
+	int m_cents;
+public:
+	Cents()
+	{
+
+	}
+
+	Cents(int cents) // Cents(int cents = 0) 으로 오버로딩 가능
+	{
+		m_cents = cents;
+	}
+
+	int getCents() const
+	{
+		return m_cents;
+	}
+
+	int &getCents()
+	{
+		return m_cents;
+	}
+};
+
+/*
+Cents add(const Cents &c1, const Cents &c2)
+{
+	return Cents (c1.getCents() + c2.getCents());
+}
+*/
+
+Cents operator + (const Cents &c1, const Cents &c2)
+{
+	return Cents(c1.getCents() + c2.getCents());
+}
+
+
+int main()
+{
+	Cents cents1(5);
+	Cents cents2(10);
+
+	/*
+	cout << add(cents1,cents2).getCents() << endl;
+	*/
+
+	cout << (cents1 + cents2 + Cents(100)).getCents() << endl; // Cents(100) -> 익명 객체 사용
+
+
+	return 0;
+}
