01a7beb2926a6b10d2a190ab822482f6e98f4619
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-01-05 22:36:36 +0900

    괄호 연산자 오버로딩 공부

---
+++ Cpp_studying/괄호_연산자_오버로딩.cpp
@@ -0,0 +1,25 @@
+﻿#include<iostream>
+
+using namespace std;
+
+class Accumulator
+{
+private:
+	int m_count = 0;
+
+public:
+	int operator() (int i) // 괄호 연산자 오버로딩, 함수 객체 처럼 사용
+	{
+		return (m_count += i);
+	}
+};
+
+int main() {
+
+	Accumulator acc;
+
+	cout << acc(10) << endl; // 10
+	cout << acc(100) << endl; // 110
+
+	return 0;
+}
