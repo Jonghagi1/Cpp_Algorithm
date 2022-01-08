22e91ff1a5d8f62989cea96d6de8aac412c5ebe7
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-01-08 14:54:49 +0900

    구성 관계 공부
    <헤더파일> Monster.h  / Position2D.h

---
+++ Cpp_studying/Monster.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+//#include<iostream>
+#include "Position2D.h"
+#include <string>
+
+using namespace std;
+
+class Monster
+{
+private:
+	std::string m_name; // char *data, unsigned lenth 묶인게 string
+	Position2D m_location; // sub class
+
+	//int m_x; // location // "Position2D.h" 로 대체
+	//int m_y;
+
+public:
+	Monster(const std::string name_in, const Position2D pos2d_in)
+		: m_name(name_in), m_location(pos2d_in)
+	{}
+
+	void moveTo(const Position2D &pos_target)
+	{
+		m_location.set(pos_target);
+	}
+
+	friend std::ostream & operator << (std::ostream &out, const Monster &monster)
+	{
+		out << monster.m_name << " " << monster.m_location;
+
+		return out;
+	}
+};
---
+++ Cpp_studying/구성_관계_Composition.cpp
@@ -0,0 +1,17 @@
+﻿#include<iostream>
+#include "Monster.h"
+
+using namespace std;
+
+int main() {
+
+	Monster mon1("Alien", Position2D(0, 0));
+
+	cout << mon1 << endl; // Alien 0 0
+
+	mon1.moveTo(Position2D(1, 7));
+
+	cout << mon1 << endl; // Alien 1 7
+
+	return 0;
+}
