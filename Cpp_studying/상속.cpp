5a8c9e2cb35fdf10c74b3510adeb8571bdfda77e
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-01-09 20:05:31 +0900

    상속 공부
    <헤더 파일> Person.h

---
+++ Cpp_studying/상속.cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+#include"Student.h"
+#include"Teacher.h"
+
+using namespace std;
+
+int main() {
+
+	Student std("KIM");
+	std.setName("KIM2");
+	cout << std.getName() << endl;
+
+	Teacher tch("Dr.K");
+	tch.setName("Dr.K2");
+	cout << tch.getName() << endl;
+
+	cout << std << endl;
+	cout << tch << endl;
+
+	cout << endl;
+
+	std.doNothing();
+	tch.doNothing();
+
+	cout << endl;
+
+	std.study();
+	tch.teach();
+
+	Person person;
+	person.setName("Park");
+	person.getName();
+	//cout << person <<endl;
+	
+	return 0;
+}
