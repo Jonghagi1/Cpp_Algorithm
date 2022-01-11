04d82eaed6edfd6d3523974aa6c604d0144322a8
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-01-11 17:55:26 +0900

    상속에서 접근 지정자 공부

---
+++ Cpp_studying/상속_접근_지정자.cpp
@@ -0,0 +1,77 @@
+﻿#include<iostream>
+
+using namespace std;
+
+class Base
+{
+public:
+	int m_public = 0;
+protected:
+	int m_protected = 1;
+private:
+	int m_private = 2;
+
+public:
+
+	void print()
+	{
+		cout << m_public << endl;
+		cout << m_protected << endl;
+		cout << m_private << endl;
+		
+		cout << endl;
+	}
+
+};
+
+class Base2 : public Base
+{
+public:
+	void print2()
+	{
+		cout << m_public << endl;
+		cout << m_protected << endl;
+		//cout << m_private << endl;
+		
+		cout << endl;
+	}
+};
+
+};
+
+class Base3 : protected Base
+{
+public:
+	void print3()
+	{
+		cout << m_public << endl;
+		cout << m_protected << endl;
+		//cout << m_private << endl;
+
+		cout << endl;
+	}
+};
+
+class Base4 : private Base
+{
+public:
+	void print3()
+	{
+		cout << m_public << endl;
+		cout << m_protected << endl;
+		//cout << m_private << endl;
+
+		cout << endl;
+	}
+};
+
+int main()
+{
+	Base base;
+	Base2 base2;
+
+	base.print();
+	base2.print2();
+
+	return 0;
+}
