2b725aa42c0e42598dc4ae7abdc465f4de82201d
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-01-13 14:29:07 +0900

    순수 가상 함수, 인터페이스 클래스 공부

---
+++ Cpp_studying/순수_가상_함수_인터페이스_클래스.cpp
@@ -0,0 +1,49 @@
+﻿#include<iostream>
+#include<string>
+
+using namespace std;
+
+class IErrorLog // Interface class
+{
+public:
+	virtual bool reportError(const char *errorMassage) = 0; // 순수 가상 함수(pure virtual function)
+
+	virtual ~IErrorLog()
+	{
+
+	}
+};
+
+class FileErrorLog : public IErrorLog
+{
+	bool reportError(const char *errorMassage) override
+	{
+		cout << "Writing error to a file" << endl;
+		return true;
+	}
+};
+
+class ConsolErrorLog : public IErrorLog
+{
+	bool reportError(const char *errorMassage) override
+	{
+		cout << "Printing error to a file" << endl;
+		return true;
+	}
+};
+
+void doSomething(IErrorLog &log)
+{
+	log.reportError("Runtime Error");
+}
+
+int main()
+{
+	FileErrorLog file_log;
+	ConsolErrorLog consol_log;
+
+	doSomething(file_log);
+	doSomething(consol_log);
+
+	return 0;
+}
