48fecae9f536b376280f057edf427748ffb12578
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-01-14 23:49:40 +0900

    함수 템플릿 특수화 공부
    <헤더파일> Storage.h

---
+++ Cpp_studying/Storage.h
@@ -0,0 +1,36 @@
+﻿#pragma once
+
+#include<iostream>
+
+using namespace std;
+
+template <class T>
+class Storage
+{
+private:
+	T m_value;
+
+public:
+	Storage(T value)
+	{
+		m_value = value;
+	}
+
+	~Storage()
+	{
+
+	}
+
+	void print()
+	{
+		cout << m_value << endl;
+	}
+};
+
+template<>
+void Storage<double>::print() // 멤버 함수를 특수화 할 떄
+{
+	cout << "Double 타입 ";
+	cout << scientific << m_value << endl;
+
+}
---
+++ Cpp_studying/함수_템플릿_특수화.cpp
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+#include "Storage.h"
+
+using namespace std;
+
+//template<typename T>
+//T getMax(T x,T y)
+//{
+//	return (x > y) ? x : y;
+//}
+//
+//template<> // 함수 템플릿 특수화
+//char getMax(char x, char y)
+//{
+//	cout << "경고 : 문자 입력" << endl;
+//
+//	return (x > y) ? x : y;
+//}
+
+int main() 
+{
+	//cout << getMax('a','b') << endl;
+
+	Storage<int> iValue(5);
+	Storage<double> dValue(6.7);
+
+	iValue.print(); // 5
+	dValue.print(); // Double 타입 6.700000e+00
+
+	return 0;
+}
