a839197695fd16baeb580ce08969ef8cf080ff58
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-01-15 14:12:23 +0900

    템플릿 내부에서 함수 특수화 공부

---
+++ Cpp_studying/템플릿_속_특수화.cpp
@@ -0,0 +1,70 @@
+﻿#include<iostream>
+
+using namespace std;
+
+template <class T, int size>
+class StaticArray_BASE
+{
+private:
+	T m_array[size];
+
+public:
+	T *getArray()
+	{
+		return m_array;
+	}
+
+	T &operator [] (int index)
+	{
+		return m_array[index];
+	}
+
+	void print()
+	{
+		for (int count = 0; count < size; count++)
+		{
+			cout << (*this)[count] << ' ';
+		}
+		cout << endl;
+	}
+};
+
+template <class T, int size>
+class StaticArray : public StaticArray_BASE<T, size>
+{
+
+};
+
+template <int size>
+class StaticArray<char,size> : public StaticArray_BASE<char, size> // 문자열부분만 특수화 // print함수 오버라이딩
+{
+public:
+	void print()
+	{
+		for (int count = 0; count < size; count++)
+		{
+			cout << (*this)[count];
+		}
+		cout << endl;
+	}
+};
+
+int main()
+{
+	StaticArray<int, 4> int4;
+
+	int4[0] = 1;
+	int4[1] = 2;
+	int4[2] = 3;
+	int4[3] = 4;
+
+	int4.print();
+
+	StaticArray<char, 14> char14;
+
+	strcpy_s(char14.getArray(), 14, "Hello, World");
+
+	char14.print();
+
+	return 0;
+}
