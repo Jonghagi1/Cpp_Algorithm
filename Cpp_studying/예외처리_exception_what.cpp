4eb2bedf5d2e16ce5f6cbf40816a5344a3b1d4f1
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-01-16 20:16:48 +0900

    예외처리 공부

---
+++ Cpp_studying/예외처리_exception_what.cpp
@@ -0,0 +1,20 @@
+﻿#include<iostream>
+#include<exception>
+#include<string>
+
+using namespace std;
+
+int main()
+{
+	try
+	{
+		string s;
+		s.resize(-1);
+	}
+	catch (const std::exception & exception)
+	{
+		cout << exception.what() << endl;
+	}
+
+	return 0;
+}
