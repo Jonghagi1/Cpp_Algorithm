3658be8167d1db928d69e03a2a8206e0406b4e8c
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-01-17 14:54:12 +0900

    std::move 이용하여 swap 해보기

---
+++ Cpp_studying/std_move_swap.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+#include<utility>	
+
+using namespace std;
+
+template<typename T>
+void Myswap(T &a, T &b)
+{
+	T temp = std::move(a);
+	a = std::move(b);
+	b = std::move(temp);
+
+}
+
+int main()
+{
+	int a = 10;
+	int b = 20;
+
+	cout << a << " " << b << endl;
+
+	Myswap(a, b);
+
+	cout << a << " " << b << endl;
+
+	return 0;
+}
