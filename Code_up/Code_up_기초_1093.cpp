ff7344ad84a1c3bfe423cb0d671d7e2ef3134235
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-01-22 21:53:29 +0900

    code up 기초 100제 1093

---
+++ Code_up/Code_up_기초_1093.cpp
@@ -0,0 +1,27 @@
+﻿#include<iostream>
+#include<stdio.h>
+
+using namespace std;
+
+int main()
+{
+	int n, index;
+
+	int arr_print[24] = { 0, }; // 0 ~ 24번 인덱스
+
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++)
+	{
+		scanf("%d", &index);
+		arr_print[index] = arr_print[index] + 1;
+	}
+
+	for (int i = 1; i <= 23; i++)
+	{
+		printf("%d ", arr_print[i]);
+	}
+
+	return 0;
+
+}
