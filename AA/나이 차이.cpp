eb96aaa04c6335283c38f27bb8363acf5d5fdc5d
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-01-25 00:14:57 +0900

    <알고리즘 공부> 나이 차이

---
+++ AA/나이 차이.cpp
@@ -0,0 +1,32 @@
+﻿#include<iostream>
+#pragma warning(disable:4996)
+
+using namespace std;
+
+int main()
+{
+	//	freopen("input.txt", "rt", stdin);	//
+
+	int a; // 입력 받을 변수
+	int n; // 입력 받을 개수
+	int max = -2147000000, min = 2147000000; // 초기 값 초기화를 위해 max는 가장 작은 값 min은 가장 큰 값으로
+
+	cin >> n;
+	
+	for (int i = 0; i < n; i++)
+	{
+		cin >> a;
+		if (a>max)
+		{
+			max = a;
+		}
+		if (a<min)
+		{
+			min = a;
+		}
+	}
+
+	cout << max - min;
+
+	return 0;
+}
