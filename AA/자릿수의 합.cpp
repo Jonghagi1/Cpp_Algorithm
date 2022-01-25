9f40c5ec3786da3e1340d4e36245f4152b291469
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-01-25 21:51:07 +0900

    <알고리즘 공부> 자릿수의 합

---
+++ AA/자릿수의 합.cpp
@@ -0,0 +1,54 @@
+﻿#include<iostream>
+#include<stdio.h>
+#pragma warning(disable:4996)
+
+using namespace std;
+
+int digit_sum(int x)
+{
+	int sum = 0, temp = 0;
+
+	while (x > 0)
+	{
+		temp = x % 10;
+		x = x / 10;
+		sum += temp;
+	}
+
+	return sum;
+}
+
+int  main()
+{
+	//freopen("input.txt", "rt", stdin);//
+
+	int n;		// 입력 받을 숫자 개수
+	int num;	// 입력 받을 숫자
+	int sum = 0, max = -2147000000;
+	int result = 0; // 결과 값 출력 변수
+
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", &num);
+		sum = digit_sum(num);
+
+		if (sum > max)
+		{
+			max = sum;
+			result = num;
+		}
+		else if (sum == max)
+		{
+			if (num > result)
+			{
+				result = num;
+			}
+		}
+	}
+
+	printf("%d", result);
+
+	return 0;
+}
