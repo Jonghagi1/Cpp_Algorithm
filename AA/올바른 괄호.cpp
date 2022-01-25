95fb086099fc9667a3363e25a80a49fe39ccd96e
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-01-25 20:09:45 +0900

    <알고리즘 공부>  올바른 괄호

---
+++ AA/올바른 괄호.cpp
@@ -0,0 +1,44 @@
+﻿#include<iostream>
+#include<stdio.h>
+#pragma warning(disable:4996)
+
+using namespace std;
+
+int main()
+{
+	//freopen("input.txt", "rt", stdin);	//
+
+	char arr[31];
+	int cnt = 0; // 카운트
+
+	scanf("%s", &arr);
+
+	for (int i = 0; arr[i] != NULL; i++)
+	{
+		if (arr[i] == '(')
+		{
+			cnt++;
+		}
+		else if (arr[i] == ')')
+		{
+			cnt--;
+		}
+
+		if (cnt < 0)
+		{
+			break;
+		}
+	}
+
+	if (cnt ==0)
+	{
+		printf("YES");
+	}
+	else
+	{
+		printf("NO");
+	}
+
+
+	return 0;
+}
