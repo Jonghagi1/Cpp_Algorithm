ffc5fd849e3af63f2262ade93555e69ae275d0c4
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-01-25 19:58:09 +0900

    <알고리즘 공부> 영어단어 복구

---
+++ AA/영어단어 복구.cpp
@@ -0,0 +1,39 @@
+﻿#include<iostream>
+#include<stdio.h>
+#pragma warning(disable:4996)
+
+using namespace std;
+
+int main()
+{
+	//freopen("input.txt", "rt", stdin);	//
+
+	char arr[101], arr2[101];
+	int p = 0; // arr2 포지션
+
+	// scanf 공백 못읽음
+	gets_s(arr);
+
+	for (int i = 0; arr[i] != NULL; i++)
+	{
+		if (arr[i] != ' ')
+		{
+			if (arr[i] >= 65 && arr[i] <=90)
+			{
+				arr2[p] = arr[i] + 32;
+				p++;
+			}
+			else
+			{
+				arr2[p] = arr[i];
+				p++;
+			}
+		}
+	}
+
+	arr2[p] = NULL;
+
+	printf("%s", arr2);
+
+	return 0;
+}
