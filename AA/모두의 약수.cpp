e0409dfabbf8c4c82e36964974db094b70adb08d
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-01-25 21:21:31 +0900

    <알고리즘 공부> 모두의 약수

---
+++ AA/모두의 약수.cpp
@@ -0,0 +1,31 @@
+﻿#include<iostream>
+#include<stdio.h>
+#pragma warning(disable:4996)
+
+using namespace std;
+
+
+int  main()
+{
+	//freopen("input.txt", "rt", stdin);	//
+
+	int n; // 숫자 개수
+	int cnt[50001] = { 0, }; // 약수 카운트
+
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = i; j <= n; j = j + i) // j + i 는 i 의 배수만큼 계산
+		{
+			cnt[j]++;
+		}
+	}
+
+	for (int i = 1; i <= n; i++)
+	{
+		printf("%d ", cnt[i]);
+	}
+
+	return 0;
+}
