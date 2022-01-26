e45ca8ff670c9c5c93d04e65e86ab9077fa7991a
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-01-26 19:35:59 +0900

    <알고리즘 공부> 숫자의 총 개수(small)

---
+++ AA/숫자의 총 개수(small).cpp
@@ -0,0 +1,36 @@
+﻿#include<iostream>
+#include<stdio.h>
+#pragma warning(disable:4996)
+
+using namespace std;
+
+int  main()
+{
+	//freopen("input.txt", "rt", stdin);//
+
+	int n, cnt = 0, temp;
+
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++)
+	{
+		temp = i;
+		while (1)
+		{
+			if (temp / 10 > 0)
+			{
+				cnt++;
+				temp = temp / 10;
+			}
+			else
+			{
+				cnt++;
+				break;
+			}
+		}
+	}
+
+	printf("%d", cnt);
+
+	return 0;
+}
