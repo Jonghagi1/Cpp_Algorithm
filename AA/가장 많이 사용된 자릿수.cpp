7cd7c50cd6dc7cf634447829f8a4f09992ab96cc
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-01-27 00:11:45 +0900

    <알고리즘 공부> 가장 많이 사용된 자릿수

---
+++ AA/가장 많이 사용된 자릿수.cpp
@@ -0,0 +1,37 @@
+﻿#include<iostream>
+#include<stdio.h>
+#pragma warning(disable:4996)
+
+using namespace std;
+
+int  main()
+{
+	//freopen("input.txt", "rt", stdin);// "input.txt"
+
+	int digit, max = -2147000000, result;
+
+	char arr[101];
+	char arr2[10];
+
+	scanf("%s", &arr);
+
+	for (int i = 0; arr[i] != NULL; i++)
+	{
+		digit = arr[i] - 48;
+		arr2[digit]++;
+	}
+
+	for (int i = 0; i < 10; i++)
+	{
+		if (arr2[i] >= max)
+		{
+			max = arr2[i]; // max 가장 많은 숫자가 있는 값
+			result = i;
+		}
+	}
+
+	printf("%d", result);
+
+
+	return 0;
+}
