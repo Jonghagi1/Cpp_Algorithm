7d106d32d1a2e4ab116165dbdf3fc486afdc4692
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-01-28 00:01:46 +0900

    <알고리즘 공부> 분노 유발자

---
+++ AA/분노 유발자.cpp
@@ -0,0 +1,38 @@
+﻿#include<iostream>
+#include<stdio.h>
+#pragma warning(disable:4996)
+
+using namespace std;
+
+
+int  main()
+{
+	//freopen("input.txt", "rt", stdin);// "input.txt"
+
+	int n; // 학생 수
+	int arr[101];
+	int max = -2147000000;
+	int cnt = 0;
+
+	scanf("%d", &n);
+
+	for (int i = 1; i <= n; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+
+	max = arr[n];
+
+	for (int i = n - 1; i >= 1; i--)
+	{
+		if (arr[i] > max)
+		{
+			max = arr[i];
+			cnt++;
+		}
+	}
+
+	printf("%d", cnt);
+
+	return 0;
+}
