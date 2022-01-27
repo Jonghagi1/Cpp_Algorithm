011324a1c6554a3ba74eb6f4c0c00a93aa24cc27
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-01-27 21:59:03 +0900

    <알고리즘 공부> 선생님 퀴즈

---
+++ AA/선생님 퀴즈.cpp
@@ -0,0 +1,51 @@
+﻿#include<iostream>
+#include<stdio.h>
+#pragma warning(disable:4996)
+
+using namespace std;
+
+
+int  main()
+{
+	//freopen("input.txt", "rt", stdin);// "input.txt"
+
+	int n; // 학생 수
+	int card, card_sum = 0;
+	int sum_check = 0;
+	int arr[11];
+
+	scanf("%d", &n);
+
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d %d", &card, &card_sum);
+		sum_check = 0;
+		for (int j = 1; j <= card; j++)
+		{
+			sum_check += j;
+		}
+
+		if (card_sum == sum_check)
+		{
+			arr[i] = 1;
+		}
+		else
+		{
+			arr[i] = 0;
+		}
+	}
+
+	for (int i = 0; i < n; i++)
+	{	
+		if (arr[i] == 1)
+		{
+			printf("YES\n");
+		}
+		if (arr[i] == 0)
+		{
+			printf("NO\n");
+		}
+	}
+
+	return 0;
+}
