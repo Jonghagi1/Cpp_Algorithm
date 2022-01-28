0854107c3a010a75b6dee6cadc8c04d43be2df66
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-01-29 02:48:22 +0900

    <알고리즘 공부> 연속 부분 증가수열

---
+++ AA/연속 부분 증가수열.cpp
@@ -0,0 +1,42 @@
+﻿#include<iostream>
+#include<stdio.h>
+#include<vector>
+#pragma warning(disable:4996)
+
+using namespace std;
+
+
+int  main()
+{
+	//freopen("input.txt", "rt", stdin);// "input.txt"
+
+	int pre, now;
+	int n;
+	int cnt = 1, max = 1;
+	
+	scanf("%d", &n);
+	scanf("%d", &pre);
+
+	for (int i = 1; i < n; i++)
+	{
+		scanf("%d", &now);
+		if (now>=pre)
+		{
+			cnt++;
+			if (cnt > max)
+			{
+				max = cnt;
+			}
+		}
+		else
+		{
+			cnt = 1;
+		}
+		pre = now;
+	}
+
+	printf("%d", max);
+	
+
+	return 0;
+}
