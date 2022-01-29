619c8ed3e9a440920fcd5b44f463643a71d15d95
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-01-29 19:31:49 +0900

    <알고리즘 공부> Jolly Jumpers

---
+++ AA/Jolly Jumpers.cpp
@@ -0,0 +1,44 @@
+﻿#include<iostream>
+#include<stdio.h>
+#include<vector>
+#include<math.h>
+#pragma warning(disable:4996)
+
+using namespace std;
+
+
+int  main()
+{
+	//freopen("input.txt", "rt", stdin);// "input.txt"
+
+	int n;
+	int pre, now, pos;
+
+	scanf("%d", &n);
+
+	vector<int> check(n);
+
+	scanf("%d", &pre); // 첫 항은 pre
+	for (int i = 1; i < n; i++)
+	{
+		scanf("%d", &now);
+
+		pos = abs(pre - now);
+
+		if (pos < n && check[pos] == 0)
+		{
+			check[pos] = 1;
+		}
+		else
+		{
+			printf("NO");
+			return 0; // 프로그램 종료
+		}
+
+		pre = now;
+	}
+
+	printf("YES");
+
+	return 0;
+}
