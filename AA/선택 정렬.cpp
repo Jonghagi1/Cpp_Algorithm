f1f055105a455274bd5b0c0aa0e23dbb4f57d555
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-01-31 17:41:25 +0900

    <알고리즘 공부> 선택 정렬

---
+++ AA/선택 정렬.cpp
@@ -0,0 +1,48 @@
+﻿#include<iostream>
+#include<stdio.h>
+#include<vector>
+#include<math.h>
+#pragma warning(disable:4996)
+
+using namespace std;
+
+
+int  main()
+{
+	freopen("input.txt", "rt", stdin);// "input.txt"
+
+	int n, temp;
+	int idx = 0;
+
+	scanf("%d", &n);
+
+	vector<int> arr(n);
+
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+
+	for (int i = 0; i < n - 1; i++)
+	{
+		idx = i;
+		for (int j = i + 1; j < n; j++)
+		{
+			if (arr[j] < arr[idx])
+			{
+				idx = j; // idx에는 가장 작은 값의 위치가 저장 된다
+			}
+		}
+
+		temp = arr[i];
+		arr[i] = arr[idx];
+		arr[idx] = temp;
+	}
+
+	for (int i = 0; i < n; i++)
+	{
+		printf("%d ", arr[i]);
+	}
+
+	return 0;
+}
