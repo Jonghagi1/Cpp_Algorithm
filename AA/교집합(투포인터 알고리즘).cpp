b25a8438cc77706342dfb4abacf5199ce85140e7
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-02-01 22:49:46 +0900

    <알고리즘 공부> 교집합(투포인터 알고리즘)

---
+++ AA/교집합(투포인터 알고리즘).cpp
@@ -0,0 +1,58 @@
+﻿#include<iostream>
+#include<stdio.h>
+#include<vector>
+#include<math.h>
+#include<algorithm>
+#pragma warning(disable:4996)
+
+using namespace std;
+
+int  main()
+{
+	//freopen("input.txt", "rt", stdin);// "input.txt"
+
+	int n, m;
+	int ap = 0, bp = 0, cp = 0;
+
+	scanf("%d", &n);
+	vector<int> a(n);
+	for (int i = 0; i < n; i++)
+	{
+		scanf("%d", &a[i]);
+	}
+	sort(a.begin(), a.end());
+
+	scanf("%d", &m);
+	vector<int> b(m);
+	for (int i = 0; i < m; i++)
+	{
+		scanf("%d", &b[i]);
+	}
+	sort(b.begin(), b.end());
+
+	vector<int> c(n + m);
+
+	while (ap < n && bp < m)
+	{
+		if (a[ap]==b[bp])
+		{
+			c[cp++] = a[ap++];
+			bp++;
+		}
+		else if(a[ap] < b[bp])
+		{
+			ap++;
+		}
+		else
+		{
+			bp++;
+		}
+	}
+
+	for (int i = 0; i < cp; i++)
+	{
+		printf("%d ", c[i]);
+	}
+
+	return 0;
+}
