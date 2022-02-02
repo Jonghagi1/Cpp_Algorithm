09f27161ed0823aa3f23a267c14e260611598551
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-02-02 16:50:18 +0900

    <알고리즘 공부> 연속된 자연수의 합

---
+++ AA/연속된 자연수의 합.cpp
@@ -0,0 +1,49 @@
+﻿#include<iostream>
+#include<stdio.h>
+#include<vector>
+#include<math.h>
+#include<algorithm>
+#pragma warning(disable:4996)
+
+using namespace std;
+
+int  main()
+{
+	//freopen("input.txt", "rt", stdin);// "input.txt"
+
+	int temp, a, b = 1;
+	int i;
+	int cnt = 0;
+
+	scanf("%d", &a); // input : 15
+
+	temp = a;
+	a--;
+	
+	while (a>0)
+	{
+		b++;
+		a = a - b;
+
+		if (a%b==0)
+		{
+			for ( i = 1; i < b; i++)
+			{
+				printf("%d + ", (a / b) + i);
+			}
+			printf("%d = %d\n", (a / b) + i, temp);
+			cnt++;
+		}
+	}
+
+	printf("%d", cnt);
+
+
+	return 0;
+}
+
+//output
+//7 + 8 + 9 = 15
+//4 + 5 + 6 + 7 = 15
+//1 + 2 + 3 + 4 + 5 + 6 = 15
+//3
