af737bda7c28422b5a98ccb61fe71565fe56723e
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-02-04 17:11:48 +0900

    <알고리즘 공부> 영지(territory)선택(v.large)

---
+++ AA/영지(territory)선택(v.large).cpp
@@ -0,0 +1,62 @@
+﻿#include<iostream>
+#include<stdio.h>
+#include<vector>
+#include<math.h>
+#include<algorithm>
+#pragma warning(disable:4996)
+
+using namespace std;
+
+int arr[701][701], dy[701][701];
+// 지역변수는 스택영역에 만들어지는데 운영체제가 스택영역을 작게 설정하기
+// 때문에 데이터 영역에 만들어지는 전역변수로 잡아야 크게 잡을 수 있다
+
+int main()
+{
+	//freopen("input.txt", "rt", stdin);// "input.txt"
+	//input
+	//6 7
+	//3 5 1 3 1 3 2
+	//1 2 1 3 1 1 2
+	//1 3 1 5 1 3 4
+	//5 1 1 3 1 3 2
+	//3 1 1 3 1 1 2
+	//1 3 1 3 1 2 2
+	//2 3
+
+
+	int h, w, n, m;
+	int temp;
+	int max = -2147000000;
+	scanf("%d %d", &h, &w);
+
+	for (int i = 1; i <= h; i++)
+	{
+		for (int j = 1; j <= w; j++)
+		{
+			scanf("%d", &arr[i][j]);
+			dy[i][j] = dy[i - 1][j] + dy[i][j - 1] - dy[i - 1][j - 1] + arr[i][j]; // 위쪽 + 왼쪽 + 겹치는부분 + 오리지널 배열테이블 값
+		}
+	}
+
+	scanf("%d %d", &n, &m);
+
+	for (int i = n; i <= h; i++)
+	{
+		for (int j = m; j <= w; j++)
+		{
+			temp = dy[i][j] - dy[i - n][j] - dy[i][j - m] + dy[i - n][j - m];
+			if (temp > max)
+			{
+				max = temp;
+			}
+		}
+	}
+
+	printf("%d", max);
+
+
+	return 0;
+}
+//output
+//16
