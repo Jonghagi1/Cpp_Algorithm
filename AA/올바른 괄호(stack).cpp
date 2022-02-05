48ace5c52e22d66baedd32cc082e701185267ee9
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-02-05 18:31:57 +0900

    <알고리즘 공부> 올바른 괄호(stack)

---
+++ AA/올바른 괄호(stack).cpp
@@ -0,0 +1,59 @@
+﻿#include<iostream>
+#include<stdio.h>
+#include<vector>
+#include<math.h>
+#include<stack>
+#include<algorithm>
+#pragma warning(disable:4996)
+
+using namespace std;
+
+
+int main()
+{
+	//freopen("input.txt", "rt", stdin);// "input.txt"
+	//input
+	//(()(()))(()
+
+	char arr[31];
+	int flag = 1;
+
+	stack<char> s;
+
+	scanf("%s", &arr);
+
+	for (int i = 0; arr[i] != NULL; i++)
+	{
+
+		if (arr[i] == '(')
+		{
+			s.push(arr[i]);
+		}
+		else
+		{
+			if (s.empty())
+			{
+				printf("NO");
+				flag = 0;
+				break;
+			}
+			else
+			{
+				s.pop();
+			}
+		}
+	}
+
+	if (s.empty() && flag == 1)
+	{
+		printf("YES");
+	}
+	else if (!s.empty() && flag == 1)
+	{
+		printf("NO");
+	}
+
+	return 0;
+}
+//output
+//NO
