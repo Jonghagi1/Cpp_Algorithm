aded9ecffccb10df0a3cad6b732382408166f4dc
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-02-05 17:40:00 +0900

    <알고리즘 공부> K진수 출력
    스택 활용

--- AA/K진수 출력.cpp
+++ AA/K진수 출력.cpp
@@ -2,22 +2,12 @@
 #include<stdio.h>
 #include<vector>
 #include<math.h>
+#include<stack>
 #include<algorithm>
 #pragma warning(disable:4996)
 
 using namespace std;
 
-int stack[100], top = -1;
-
-void push(int x)
-{
-	stack[++top] = x;
-}
-
-int pop()
-{
-	return stack[top--];
-}
 
 int main()
 {
@@ -26,19 +16,21 @@ int main()
 	//31 16
 
 	int n, k;
+	stack<int> s;
 	char str[20] = "0123456789ABCDEF";
 
 	scanf("%d %d", &n, &k);
 
 	while (n>0)
 	{
-		push(n % k);
+		s.push(n % k);
 		n = n / k;
 	}
 
-	while (top != -1)
+	while (!s.empty())
 	{
-		printf("%c", str[pop()]);
+		printf("%c", str[s.top()]);
+		s.pop();
 	}
 
 	return 0;
