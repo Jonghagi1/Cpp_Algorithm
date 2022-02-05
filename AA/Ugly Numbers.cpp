0643f3b051f73e6a7e61d77eee9f842e72d063a8
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-02-05 15:06:06 +0900

    <알고리즘 공부> Ugly Numbers

---
+++ AA/Ugly Numbers.cpp
@@ -0,0 +1,56 @@
+﻿#include<iostream>
+#include<stdio.h>
+#include<vector>
+#include<math.h>
+#include<algorithm>
+#pragma warning(disable:4996)
+
+using namespace std;
+
+int arr[1501];
+
+int main()
+{
+	//freopen("input.txt", "rt", stdin);// "input.txt"
+	//input
+	//10
+
+	int n;
+	int p2, p3, p5;
+	int min = 2147000000;
+	int i;
+
+	scanf("%d", &n);
+
+	arr[1] = 1;
+	p2 = p3 = p5 = 1; // p2*2, p3*3, p5*5
+
+	for (int i = 2; i <= n; i++)
+	{
+		if (arr[p2] * 2 < arr[p3] * 3)
+		{
+			min = arr[p2] * 2;
+		}
+		else
+		{
+			min = arr[p3] * 3;
+		}
+
+		if (arr[p5] * 5 < min)
+		{
+			min = arr[p5] * 5;
+		}
+
+		if (arr[p2] * 2 == min) p2++;
+		if (arr[p3] * 3 == min) p3++;
+		if (arr[p5] * 5 == min) p5++;
+
+		arr[i] = min;
+	}
+
+	printf("%d", arr[n]);
+
+	return 0;
+}
+//output
+//12
