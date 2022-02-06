fa9c66e91ee2c33e362a069f17ff47f7630639a2
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-02-06 13:16:54 +0900

    <알고리즘 공부> 재귀함수 이진수 출력

---
+++ AA/재귀함수 이진수 출력.cpp
@@ -0,0 +1,39 @@
+﻿#include<iostream>
+#include<stdio.h>
+#include<vector>
+#include<math.h>
+#include<stack>
+#include<algorithm>
+#pragma warning(disable:4996)
+
+using namespace std;
+
+void D(int n)
+{
+	if (n==0)
+	{
+		return;
+	}
+	else
+	{
+		D(n / 2);
+		printf("%d", n % 2);
+	}
+}
+
+int main()
+{
+	//freopen("input.txt", "rt", stdin);// "input.txt"
+	//input
+	//11
+
+	int n;
+	
+	scanf("%d", &n);
+
+	D(n);
+
+	return 0;
+}
+//output
+//1011
