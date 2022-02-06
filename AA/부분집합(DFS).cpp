9673f6712df076e5b932588ccdffe3becb44ea0d
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-02-06 14:20:40 +0900

    <알고리즘 공부> 부분집합(DFS)

---
+++ AA/부분집합(DFS).cpp
@@ -0,0 +1,54 @@
+﻿#include<iostream>
+#include<stdio.h>
+#include<vector>
+#include<math.h>
+#include<stack>
+#include<algorithm>
+#pragma warning(disable:4996)
+
+using namespace std;
+
+int ch[11];
+int n;
+void DFS(int L)
+{
+	if (L == n + 1)
+	{
+		for (int i = 1; i <= n; i++)
+		{
+			if (ch[i] == 1)
+			{
+				printf("%d ", i);
+			}
+		}
+		printf("\n");
+	}
+	else
+	{
+		ch[L] = 1;
+		DFS(L + 1);
+		ch[L] = 0;
+		DFS(L + 1);
+	}
+}
+
+//input
+//3
+int main()
+{
+	//freopen("input.txt", "rt", stdin);// "input.txt"
+
+	scanf("%d", &n);
+
+	DFS(11);
+
+	return 0;
+}
+//output
+//1 2 3
+//1 2
+//1 3
+//1
+//2 3
+//2
+//3
