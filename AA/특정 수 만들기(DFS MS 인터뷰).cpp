00edc6996e8d0cb48e5487a1f3f31eae826d04d2
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-02-07 16:44:46 +0900

    <알고리즘 공부> 특정 수 만들기(DFS MS 인터뷰)

---
+++ AA/특정 수 만들기(DFS MS 인터뷰).cpp
@@ -0,0 +1,62 @@
+﻿#include<iostream>
+#include<stdio.h>
+#include<vector>
+#include<math.h>
+#include<stack>
+#include<algorithm>
+#pragma warning(disable:4996)
+
+using namespace std;
+
+//input
+//4 12
+//2 4 6 8
+
+int n, m;
+int cnt = 0;
+int arr[11];
+
+void DFS(int L, int Val)
+{
+	if (L == n + 1)
+	{
+		if (m == Val)
+		{
+			cnt++;
+		}
+	}
+	else
+	{
+		DFS(L + 1, Val + arr[L]);
+		DFS(L + 1, Val - arr[L]);
+		DFS(L + 1, Val);
+	}
+}
+
+int main()
+{
+	//freopen("input.txt", "rt", stdin);// "input.txt"
+
+	scanf("%d %d", &n, &m);
+
+	for (int i = 1; i <= n; i++)
+	{
+		scanf("%d", &arr[i]);
+	}
+
+	DFS(1, 0);
+
+	if (cnt == 0)
+	{
+		printf("-1");
+	}
+	else
+	{
+		printf("%d", cnt);
+	}
+
+
+	return 0;
+}
+//output
+//4
