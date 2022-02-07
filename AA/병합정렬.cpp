d31918c21c8f13b0d30c1f338ef8d237f0107f29
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-02-07 21:54:21 +0900

    <알고리즘 공부> 병합정렬

---
+++ AA/병합정렬.cpp
@@ -0,0 +1,37 @@
+﻿#include<iostream>
+#include<stdio.h>
+#include<vector>
+#include<math.h>
+#include<stack>
+#include<algorithm>
+#pragma warning(disable:4996)
+
+using namespace std;
+
+//input
+//6 9
+//1 2 7
+//1 3 4
+//2 1 2
+//2 3 5
+//2 5 5
+//3 4 5
+//4 2 2
+//4 5 5
+//6 4 5
+
+int main()
+{
+	//freopen("input.txt", "rt", stdin);// "input.txt"
+
+
+
+	return 0;
+}
+//output
+//0 7 4 0 0 0
+//2 0 5 0 5 0
+//0 0 0 5 0 0
+//0 2 0 0 5 0
+//0 0 0 0 0 0
+//0 0 0 5 0 0
