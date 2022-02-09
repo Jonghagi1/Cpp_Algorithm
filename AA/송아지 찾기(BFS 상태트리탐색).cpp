8a05ca59e0c3a94698c9f088177e95b159330540
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-02-09 16:33:05 +0900

    <알고리즘 공부> 송아지 찾기(BFS 상태트리탐색)

---
+++ AA/송아지 찾기(BFS 상태트리탐색).cpp
@@ -0,0 +1,59 @@
+﻿#include<stdio.h>
+#include<vector>
+#include<queue>
+#include<algorithm>
+#pragma warning(disable:4996)
+
+using namespace std;
+
+//input
+//5 14
+
+int ch[10001];
+int d[3] = { 1, -1, 5 };
+
+int main()
+{
+	//freopen("input.txt", "rt", stdin);// "input.txt"
+
+	int n, m;
+	queue<int> Q;
+	int x;
+	int pos = 0;
+
+	scanf("%d %d", &n, &m);
+
+	Q.push(n);
+	ch[n] = 1; // 출발지점
+
+	while (!Q.empty())
+	{
+		x = Q.front();
+		Q.pop();
+
+		for (int i = 0; i < 3; i++)
+		{
+			pos = x + d[i];
+			if (pos <= 0 || pos > 10000) { continue; }
+			if (pos == m)
+			{
+				printf("%d", ch[x]); // ch[n] = 1 처음이 1로 시작했기 때문에 그 전 지점인 x를 출력 해야한다
+				exit(0);
+			}
+			else
+			{
+				if (ch[pos] == 0)
+				{
+					ch[pos] = ch[x] + 1;
+					Q.push(pos);
+				}
+			}
+
+		}
+
+	}
+
+	return 0;
+}
+//output
+//3
