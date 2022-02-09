9bdedb2af2a93e9e4b04298e77fdc286b624f3fc
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-02-09 16:49:08 +0900

    <알고리즘 공부> 공주 구하기(큐 자료구조로 해결)

---
+++ AA/공주 구하기(큐 자료구조로 해결).cpp
@@ -0,0 +1,49 @@
+﻿#include<queue>
+#include<stdio.h>
+#include<vector>
+#include<algorithm>
+#pragma warning(disable:4996)
+
+using namespace std;
+
+//input
+//8 3
+
+int main()
+{
+	//freopen("input.txt", "rt", stdin);// "input.txt"
+
+	int n, m;
+	int k = 0;
+	queue<int> Q;
+	int x;
+	
+	scanf("%d %d", &n, &m);
+
+	for (int i = 1; i <= n; i++)
+	{
+		Q.push(i);
+	}
+
+	while (!Q.empty())
+	{
+		for (int i = 1; i < m; i++)
+		{
+			Q.push(Q.front());
+			Q.pop();
+		}
+		Q.pop();
+
+		if (Q.size() == 1)
+		{
+			printf("%d", Q.front());
+			Q.pop();
+		}
+
+	}
+
+
+	return 0;
+}
+//output
+//7
