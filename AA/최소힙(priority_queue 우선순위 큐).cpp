491f021f2a6df61d55b26f9e59302a2e64ee5fcd
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-02-10 14:27:59 +0900

    <알고리즘 공부> 최소힙(priority_queue 우선순위 큐)

---
+++ AA/최소힙(priority_queue 우선순위 큐).cpp
@@ -0,0 +1,63 @@
+﻿#include<queue>
+#include<stdio.h>
+#include<vector>
+#include<algorithm>
+#pragma warning(disable:4996)
+
+using namespace std;
+
+//input
+//5
+//3
+//6
+//0
+//5
+//0
+//2
+//4
+//0
+//-1
+
+
+int main()
+{
+	//freopen("input.txt", "rt", stdin);// "input.txt"
+
+	int n;
+
+	priority_queue<int> pQ;
+
+	while (1)
+	{
+		scanf("%d", &n);
+
+		if (n == -1)
+		{
+			break;
+		}
+
+		if (n == 0)
+		{
+			if (pQ.empty())
+			{
+				printf("-1");
+			}
+			else
+			{
+				printf("%d\n", -pQ.top());
+				pQ.pop();
+			}
+		}
+		else
+		{
+			pQ.push(-n);
+		}
+
+	}
+
+	return 0;
+}
+//output
+//3
+//5
+//2
