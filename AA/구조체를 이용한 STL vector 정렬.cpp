29e02298c3662345608163668dddfcdf97a8ebbb
DESKTOP-MU5FO22\admin <jonghagi1@naver.com>
2022-02-10 15:56:23 +0900

    <알고리즘 공부> 구조체를 이용한 STL vector 정렬

---
+++ AA/구조체를 이용한 STL vector 정렬.cpp
@@ -0,0 +1,57 @@
+﻿#include<queue>
+#include<stdio.h>
+#include<vector>
+#include<algorithm>
+#include<iostream>
+#pragma warning(disable:4996)
+
+using namespace std;
+
+//input
+
+struct Loc {
+	int x, y, z;
+
+	Loc(int a, int b, int c)
+	{
+		x = a;
+		y = b;
+		z = c;
+	}
+
+	bool operator< (const Loc &o) const
+	{
+		if (x!=o.x)
+		{
+			return x < o.x;
+		}
+		if (y!=o.y)
+		{
+			return y<o.y;
+		}
+		if (z != o.z)
+		{
+			return z < o.z;
+		}
+	}
+};
+
+int main()
+{
+	//freopen("input.txt", "rt", stdin);// "input.txt"
+
+	vector<Loc> xy;
+	xy.push_back(Loc(2, 3, 5));
+	xy.push_back(Loc(3, 6, 7));
+	xy.push_back(Loc(2, 3, 1));
+	xy.push_back(Loc(5, 2, 3));
+	xy.push_back(Loc(3, 1, 6));
+
+	sort(xy.begin(), xy.end());	
+
+	for (auto pos : xy) cout << pos.x << " " << pos.y << " " << pos.z << endl;
+
+
+	return 0;
+}
+//output
